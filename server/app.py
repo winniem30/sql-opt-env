b989cfa90049d4c6cb5aebb9609a8f42ddc2a98c
Tejaswini Medandrao <147242711+winniem30@users.noreply.github.com>
2026-04-10 10:38:36 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,12 @@
-# server/app.py
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from server import app  # noqa: F401
+import uvicorn
+from server import app
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
